8e4900f0d885425df56bf15982cd3fff34272cf6
MasumRaj <promotionbd321@gmail.com>
2024-05-24 11:38:35 +0600

    signUp api start

---
+++ lib/services/api_check.dart
@@ -0,0 +1,20 @@
+import 'package:get/get.dart';
+import 'package:k9academy/core/app_routes/app_routes.dart';
+import 'package:k9academy/helper/shared_prefe/shared_prefe.dart';
+import 'package:k9academy/utils/app_const/app_const.dart';
+import 'package:k9academy/utils/toast_message/toast_message.dart';
+
+
+class ApiChecker {
+  static void checkApi(Response response, {bool getXSnackBar = false}) async {
+    if (response.statusCode == 401) {
+      await SharePrefsHelper.remove(AppConstants.bearerToken);
+
+      await SharePrefsHelper.setBool(AppConstants.rememberMe, false);
+
+      Get.offAllNamed(AppRoute.onBoarding);
+    } else {
+      showCustomSnackBar(response.statusText!, getXSnackBar: getXSnackBar);
+    }
+  }
+}
---
+++ lib/services/app_url.dart
@@ -0,0 +1,7 @@
+class AppUrl {
+  ///=================================This is baseUrl==============================================>
+  static const baseUrl = "http://157.245.97.245:5001";
+
+  static const forgetAuth = "/api/v1/otps/forgot-passwordOtp";
+
+}
