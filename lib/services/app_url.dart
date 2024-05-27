de6ec43478a7ab16f646c4e7bebd57be44a2d092
MasumRaj <promotionbd321@gmail.com>
2024-05-27 10:20:57 +0600

    authentication complete

--- lib/services/app_url.dart
+++ lib/services/app_url.dart
@@ -1,13 +1,21 @@
 class AppUrl {
   ///=================================This is baseUrl==============================================>
   static const baseUrl = "http://192.168.10.116:5000";
-
+///=================================Authentication url==========================
   static const signupAuth = "/auth/register";
   static const otpVerificationEndPoint = "/auth/activate-user";
   static const logIn = "/auth/login";
   static const forgetOtp = "/auth/forgot-password";
-  static const changePassword = "/auth/change-password";
   static const forgetOtpVerify = "/auth/verify-otp";
   static const resetPassword = "/auth/reset-password";
-  static const resetOtpSignUp = "/auth/resend";
+  static const resetOtpSignUp = "/auth/resend";       //state problem
+
+
+  ///===================================Profile url===============================
+  static const getProfile = "/auth/profile/";         //not complete
+  static const editProfile = "/auth/edit-profile/";  //not complete
+
+  ///====================================Settings url================================
+  static const changePassword = "/auth/change-password";  //not complete
+  static const deleteAccount = "/auth/delete-account";    //not complete
 }
